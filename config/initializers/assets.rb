3aca1c18c8688d7ce3e1009552089221114a57aa
pandamouse <pandamouse@gmail.com>
2015-04-05 09:26:34 +1000

    require instead of import

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,10 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths += Dir[Rails.root.join('vendor', 'assets', 'bower_components').to_s]
+Rails.application.config.assets.paths += Dir[Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts").to_s]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
